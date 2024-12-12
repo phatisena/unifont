f9901bae29514c6cba1a5dddff93e83cc00b903f
Phatisena <naphatism@hotmail.com>
2024-12-12 14:15:13 +0700

    Create main-kind.ts

---
+++ main-kind.ts
@@ -0,0 +1,15 @@
+namespace unistat {
+  /**
+     * A type of state to get for a player
+     */
+    //% shim=KIND_GET
+    //% blockId=mp_multiplayerstate
+    //% block="$kind"
+    //% kindNamespace=MultiplayerState
+    //% kindMemberName=value
+    //% kindPromptHint="e.g. Cooldown, Speed, Attack..."
+    //% blockHidden
+    export function _multiplayerState(kind: number): number {
+        return kind;
+    }
+}
